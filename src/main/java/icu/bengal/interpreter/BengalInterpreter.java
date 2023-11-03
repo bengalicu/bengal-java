53671304be0dd2982dd6bd1ac8674680af99e8c4
Automated workflow <noreply@piranha.cloud>
2023-11-03 07:52:12 -0500

    Fixes #3 - Create a GraalVM binary

---
+++ src/main/java/icu/bengal/interpreter/BengalInterpreter.java
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (c) 2002-2023, Manorrock.com. All Rights Reserved.
+ */
+package icu.bengal.interpreter;
+
+/**
+ * The Bengal Interpreter.
+ * 
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class BengalInterpreter {
+    
+    /**
+     * Main method.
+     * 
+     * @param arguments the arguments.
+     */
+    public static void main(String[] arguments) {
+        System.out.println("Bengal Interpreter");
+    }
+}
